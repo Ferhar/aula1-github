d9ecd06fcd884aa8a050536a38b260effd302b31
Ferhar <fernando.henrique_93@hotmail.com>
2020-01-12 16:30:09 -0300

    FileReader e BufferedReader declaro no bloco Try

--- src/application/File_aula.java
+++ src/application/File_aula.java
@@ -9,33 +9,15 @@ public class File_aula {
 	public static void main(String[] args) {
 
 		String path = "C:\\Users\\ferna\\OneDrive\\Área de Trabalho\\AulaJava\\in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
-
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
-
 			while (line != null) {
 				System.out.println(line);
 				line = br.readLine();
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		} finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
-
 	}
 }
